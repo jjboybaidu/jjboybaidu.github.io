027deccd5be0ca81e37de54ba7c52e73d67c122b
jjboybaidu <jjboybaidu@gmail.com>
2016-08-14 09:10:58 +0800

    Site updated: 2016-08-14 09:10:57

--- js/script.js
+++ js/script.js
@@ -1,149 +1,91 @@
-(function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
-
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
-
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
-  });
-
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
-
-  // Share
-  /*$('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
-
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-			'<a href="http://tieba.baidu.com/f/commit/share/openShareApi?url=' + encodedUrl + '" class="article-share-tieba" target="_blank" title="百度贴吧"></a>',
-			'<a href="http://service.weibo.com/share/share.php?url=' + encodedUrl + '" class="article-share-weibo" target="_blank" title="新浪微博"></a>',
-			'<a href="http://share.v.t.qq.com/index.php?c=share&a=index&url=' + encodedUrl + '" class="article-share-tqq" target="_blank" title="腾讯微博"></a>',
-			'<a href="http://widget.renren.com/dialog/share?resourceUrl=' + encodedUrl + '" class="article-share-renren" target="_blank" title="人人"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
-    }
-
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });*/
-
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
-
-      var alt = this.alt;
-
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
-    });
-
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
-  });
-
-  if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
-
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
-
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
+(function($) {
   
-    // link
-    var $linkUl = $('#link-list');
-    var $list = $('#link-list li');
-    $linkUl.empty();
-    var count = $list.length;
-    for(var i = 0; i < count; i++)
-    {
-        var ran = Math.floor(Math.random() * $list.length);
-        $linkUl.append($list.eq(ran));
-        $list.splice(ran, 1);
+  // scroll-spy
+  $('body').scrollspy({ target: '#navbar-toc' });
+  $('#toc').css('min-width', $('#navbar-toc').css('width'));
+  
+  // Article
+  $('.article').each(function(i) {
+    // image
+    $(this).find('img').each(function() {
+      if ($(this).parent().hasClass('fancybox-button')) return;
+      var alt = this.alt;
+      if (alt) $(this).after('<span class="funcybox-caption">' + alt + '</span>');
+
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox-button"></a>');
+    });
+
+    $(this).find('.fancybox-button').each(function(j) {
+      $(this).attr('rel', 'fancybox-button');
+    });
+
+    // table
+    $(this).find('table').each(function() {
+      if (!$(this).hasClass('table-bordered')) {
+        $(this).addClass('table');
+        $(this).addClass('table-bordered');
+      }
+    });
+  });
+
+  if ($.fancybox) {
+    $('.fancybox-button').fancybox({
+      helpers : {
+        title : {
+          type : 'over'
+        },
+        buttons : {},
+        overlay : {
+          css : {
+            'background' : 'rgba(58, 42, 45, 0.5)'
+          }
+        }
+      },
+      afterShow : function() {
+        /* Add watermark */
+        $('<div class="watermark"></div>').bind("contextmenu", function(e) {
+          return false; /* Disables right click */
+        }).prependTo($.fancybox.inner);
+
+        $(".fancybox-title").wrapInner('<div />').show();
+
+        $(".fancybox-wrap").hover(function() {
+          $(".fancybox-title").show();
+        }, function() {
+          $(".fancybox-title").hide();
+        });
+      }
+    });
+  }
+
+  $.fn.chk_userlanguage = function() {
+    /* check if <style=display:none;> not set to that element */
+    // if (!this.is(":hidden")) { this.hide(); };
+    /* get browser default lang */
+    if (navigator.userLanguage) {
+      baseLang = navigator.userLanguage.substring(0, 2).toLowerCase();
     }
+    else {
+      baseLang = navigator.language.substring(0, 2).toLowerCase();
+    }
+
+    /* language match */
+    switch (baseLang) {
+    case "de":
+      /* german */
+      this.slideDown("slow");
+      break;
+    case "en":
+      /* english */
+      break;
+    case "ja":
+      /* japanese */
+      break;
+    case "zh":
+      /* 中文 */
+      break;
+    default:
+      /* default no match */
+    }
+  };
 })(jQuery);
